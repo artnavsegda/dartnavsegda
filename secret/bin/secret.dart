ddbcc64362c59e9f10f9a61b55c0a77912211488
Art Navsegda <artnavsegda@gmail.com>
2024-06-07 21:59:50 +0300

    Update secret.dart

--- secret/bin/secret.dart
+++ secret/bin/secret.dart
@@ -1,5 +1,10 @@
 import 'package:secret/secret.dart' as secret;
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
 
 void main(List<String> arguments) {
-  print('Hello world: ${secret.calculate()}!');
+  var bytes = utf8.encode("foobar"); // data being hashed
+  var digest = sha256.convert(bytes);
+
+  print('Hello world: ${digest}!');
 }
