f20de00a049db093b633018fe3199f7d284e74ed
Art Navsegda <artnavsegda@gmail.com>
2024-08-15 21:35:33 +0300

    Update ffihello.dart

--- ffihello/bin/ffihello.dart
+++ ffihello/bin/ffihello.dart
@@ -1,6 +1,13 @@
 import 'package:ffihello/ffihello.dart' as ffihello;
-import 'dart:ffi';
+import 'dart:ffi' as ffi;
+
+typedef HelloWorld = void Function();
+typedef hello_world_func = ffi.Void Function();
 
 void main(List<String> arguments) {
   print('Hello world: ${ffihello.calculate()}!');
+  final dylib = ffi.DynamicLibrary.open("hello.dll");
+  final HelloWorld hello = dylib
+      .lookup<ffi.NativeFunction<hello_world_func>>('hello_world')
+      .asFunction();
 }
