49288d52ed4a6b83505f4861843963354062d094
Art Navsegda <artnavsegda@gmail.com>
2024-08-17 22:07:43 +0300

    Update ffihello.dart

--- ffihello/bin/ffihello.dart
+++ ffihello/bin/ffihello.dart
@@ -5,13 +5,22 @@ import 'package:ffi/ffi.dart';
 typedef FptrCreateFunc = Void Function(Pointer<Void> handle);
 typedef FptrCreate = void Function(Pointer<Void> handle);
 
+typedef FptrGetVersionStrigFunc = Void Function();
+typedef FptrGetVersionStrig = void Function();
+
 void main(List<String> arguments) {
   print('Hello world: ${ffihello.calculate()}!');
   final dylib = DynamicLibrary.open(
       "C:\\Program Files\\ATOL\\Drivers10\\KKT\\bin\\fptr10.dll");
-  final fptr = calloc<Void>();
   final FptrCreate fptrCreate = dylib
       .lookup<NativeFunction<FptrCreateFunc>>('libfptr_create')
       .asFunction();
+  final FptrGetVersionStrig fptrGetVersionString = dylib
+      .lookup<NativeFunction<FptrGetVersionStrigFunc>>(
+          'libfptr_get_version_string')
+      .asFunction();
+
+  Pointer<Void> fptr = calloc<Void>();
   fptrCreate(fptr);
+  fptrGetVersionString();
 }
