5d885bfd3fcdcfa05c271e71a02c6f16a4df90c9
Art Navsegda <artnavsegda@gmail.com>
2024-08-17 21:13:05 +0300

    Update ffihello.dart

--- ffihello/bin/ffihello.dart
+++ ffihello/bin/ffihello.dart
@@ -2,16 +2,16 @@ import 'package:ffihello/ffihello.dart' as ffihello;
 import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 
-typedef HelloWorldFunc = Void Function(Pointer<Void> handle);
-typedef HelloWorld = void Function(Pointer<Void> handle);
+typedef FptrCreateFunc = Void Function(Pointer<Void> handle);
+typedef FptrCreate = void Function(Pointer<Void> handle);
 
 void main(List<String> arguments) {
   print('Hello world: ${ffihello.calculate()}!');
   final dylib = DynamicLibrary.open(
       "C:\\Program Files\\ATOL\\Drivers10\\KKT\\bin\\fptr10.dll");
   final fptr = calloc<Void>();
-  final HelloWorld hello = dylib
-      .lookup<NativeFunction<HelloWorldFunc>>('libfptr_create')
+  final FptrCreate fptrCreate = dylib
+      .lookup<NativeFunction<FptrCreateFunc>>('libfptr_create')
       .asFunction();
-  hello(fptr);
+  fptrCreate(fptr);
 }
