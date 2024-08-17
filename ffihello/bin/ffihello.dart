a7f4342a4e7cc44215368b7487d5311bb92f1daa
Art Navsegda <artnavsegda@gmail.com>
2024-08-17 20:41:38 +0300

    sfd

--- ffihello/bin/ffihello.dart
+++ ffihello/bin/ffihello.dart
@@ -6,8 +6,10 @@ typedef hello_world_func = ffi.Void Function();
 
 void main(List<String> arguments) {
   print('Hello world: ${ffihello.calculate()}!');
-  final dylib = ffi.DynamicLibrary.open("C:\\sc552\\pilot_nt.dll");
+  final dylib = ffi.DynamicLibrary.open(
+      "C:\\Program Files\\ATOL\\Drivers10\\KKT\\bin\\fptr10.dll");
   final HelloWorld hello = dylib
-      .lookup<ffi.NativeFunction<hello_world_func>>('_card_authorize9')
+      .lookup<ffi.NativeFunction<hello_world_func>>('libfptr_create')
       .asFunction();
+  hello();
 }
